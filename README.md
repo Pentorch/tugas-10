79856744e548dcca42463f4a9598e50caf857543
WillyAgustinoEfendi <59289325+Pentorch@users.noreply.github.com>
2021-08-04 13:00:38 +0700

    update global

---
+++ README.md
@@ -0,0 +1,20 @@
+# Tugas-10
+
+Tampilan Create:
+![pictures](assets/img/Create.png)
+<br/>
+<br/>
+Tampilan Read:
+![pictures](assets/img/Dashboard.png)
+<br/>
+<br/>
+Tampilan Update:
+![pictures](assets/img/Update.png)
+<br/>
+<br/>
+Tampilan Delete:
+![pictures](assets/img/Delete.png)
+<br/>
+Tampilan Delete:
+![pictures](assets/img/Delete1.png)
+<br/>
---
+++ db/arkademy.sql
@@ -0,0 +1,70 @@
+-- phpMyAdmin SQL Dump
+-- version 4.9.0.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Waktu pembuatan: 04 Agu 2021 pada 07.52
+-- Versi server: 10.4.6-MariaDB
+-- Versi PHP: 7.3.9
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `arkademy`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `produk`
+--
+
+CREATE TABLE `produk` (
+  `id` int(11) NOT NULL,
+  `nama_produk` varchar(50) NOT NULL,
+  `keterangan` text NOT NULL,
+  `harga` varchar(50) NOT NULL,
+  `jumlah` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data untuk tabel `produk`
+--
+
+INSERT INTO `produk` (`id`, `nama_produk`, `keterangan`, `harga`, `jumlah`) VALUES
+(7, 'Eiger1', 'New', '4000', '2'),
+(8, 'Marimas2', 'New', '4000', '2');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indeks untuk tabel `produk`
+--
+ALTER TABLE `produk`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT untuk tabel yang dibuang
+--
+
+--
+-- AUTO_INCREMENT untuk tabel `produk`
+--
+ALTER TABLE `produk`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
